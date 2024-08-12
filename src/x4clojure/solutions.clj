09f23e5a10718cdcbca91910d10050a840978ed3
Mario Giampietri <mario.giampietri@gmail.com>
2024-08-12 22:41:47 +0100

    4clojure: fibonacci & flatten

--- src/x4clojure/solutions.clj
+++ src/x4clojure/solutions.clj
@@ -10,9 +10,22 @@
       (let [next-fibo (apply + (take 2 (or (seq results) '(1))))]
         (recur (dec idx) (cons next-fibo results))))))
 
-(defn solution26 [n] (fibo n))
-
 (comment
 ; clever solution, assumes result starting from 0
-
   (def fibs (lazy-cat [0N 1N] (map + fibs (rest fibs)))))
+
+; Problem 28, Flatten a Sequence
+(defn my-flatten [[x & xs]]
+  (when (some? x)
+    (if (not (sequential? x))
+      (cons x (trampoline my-flatten xs))
+      (concat (trampoline my-flatten x) (trampoline my-flatten xs)))))
+
+(comment
+; what was submitted was this to make the __ stuff work
+(letfn [(my-flatten [[x & xs]]
+  (when (some? x)
+    (if (not (sequential? x))
+      (cons x (trampoline my-flatten xs))
+      (concat (trampoline my-flatten x) (trampoline my-flatten xs)))))]
+#(my-flatten %)))
--- test/x4clojure/solutions_test.clj
+++ test/x4clojure/solutions_test.clj
@@ -4,6 +4,11 @@
     [x4clojure.solutions :refer :all]))
 
 (deftest problem26-fibonacci
-  (is (= (solution26 3) '(1 1 2)))
-  (is (= (solution26 6) '(1 1 2 3 5 8)))
-  (is (= (solution26 8) '(1 1 2 3 5 8 13 21))))
+  (is (= (fibo 3) '(1 1 2)))
+  (is (= (fibo 6) '(1 1 2 3 5 8)))
+  (is (= (fibo 8) '(1 1 2 3 5 8 13 21))))
+
+(deftest problem28-flatten-a-sequence
+  (is (= (my-flatten '((1 2) 3 [4 [5 6]])) '(1 2 3 4 5 6)))
+  (is (= (my-flatten ["a" ["b"] "c"]) '("a" "b" "c")))
+  (is (= (my-flatten '((((:a))))) '(:a))))
