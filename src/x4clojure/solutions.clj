fa03f579eb942e9ab51b2d3201a2f1add32764b0
Mario Giampietri <mario.giampietri@gmail.com>
2024-08-16 22:59:45 +0100

    comments to docstrings

--- src/x4clojure/solutions.clj
+++ src/x4clojure/solutions.clj
@@ -2,8 +2,9 @@
 
 ; Solutions to 4clojure problems. Not all of them, skipping trivial ones
 
-; Problem 26, Fibonacci Sequence
-(defn fibo [n]
+(defn fibo
+  "Problem 26, Fibonacci Sequence"
+  [n]
   (loop [idx n results []]
     (if (zero? idx)
       (into () results)
@@ -14,9 +15,10 @@
 ; clever solution, assumes result starting from 0
   (def fibs (lazy-cat [0N 1N] (map + fibs (rest fibs)))))
 
-; Problem 28, Flatten a Sequence
-(defn my-flatten [coll]
-    (if (sequential? coll) (mapcat my-flatten coll) [coll]))
+(defn my-flatten
+  "Problem 28, Flatten a Sequence"
+  [coll]
+  (if (sequential? coll) (mapcat my-flatten coll) [coll]))
 
 (comment
 ; submitted as
@@ -24,8 +26,9 @@
             (if (sequential? coll) (mapcat my-flatten coll) [coll]))]
     #(my-flatten %)))
 
-; Problem 29, Get the Caps
-(defn get-the-caps [text]
+(defn get-the-caps
+  "Problem 29, Get the Caps"
+  [text]
   (apply str (filter #(Character/isUpperCase %) text)))
 
 (comment
@@ -33,25 +36,31 @@
   (fn [s]
     (apply str (filter #(contains? (set "ABCDEFGHIJKLMNOPQRSTUVWXYZ") %) s))))
 
-; Problem 30, Compress a Sequence
-(defn my-dedupe [xs]
+(defn my-dedupe
+  "Problem 30, Compress a Sequence"
+  [xs]
   (reduce
    (fn [res curr] (if (= curr (last res)) res (conj res curr)))
    [] xs))
 
-; Problem 32, Duplicate a Sequence
-(defn duplicate-seq [xs] (mapcat #(vector % %) xs))
+(defn duplicate-seq
+  "Problem 32, Duplicate a Sequence"
+  [xs]
+  (mapcat #(vector % %) xs))
 
 (comment
 ; clever!
   (fn [[& args]] (reduce #(conj %1 %2 %2) [] args)))
 
-; Problem 34, Implement range
-(defn my-range [x y]
+(defn my-range
+  "Problem 34, Implement range"
+  [x y]
   (loop [curr x res []]
     (if (= curr y)
        res
        (recur (inc curr) (conj res curr)))))
 
-; Problem 39, Interleave Two Seq
-(defn my-interleave [c1 c2] (mapcat #(vector %1 %2) c1 c2))
+(defn my-interleave
+  "Problem 39, Interleave Two Seq"
+  [c1 c2]
+  (mapcat #(vector %1 %2) c1 c2))
