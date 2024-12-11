df12604cdad76588bf53d86f97199e365a21f460
Mario Giampietri <mario.giampietri@gmail.com>
2024-12-11 17:22:47 +0000

    dashes

--- test/x4clojure/solutions_test.clj
+++ test/x4clojure/solutions_test.clj
@@ -3,12 +3,12 @@
     [clojure.test :refer :all]
     [x4clojure.solutions :refer :all]))
 
-(deftest problem26-fibonacci
+(deftest problem-26-fibonacci
   (is (= (fibo 3) '(1 1 2)))
   (is (= (fibo 6) '(1 1 2 3 5 8)))
   (is (= (fibo 8) '(1 1 2 3 5 8 13 21))))
 
-(deftest problem28-flatten-a-sequence
+(deftest problem-28-flatten-a-sequence
   (is (= (my-flatten '((1 2) 3 [4 [5 6]])) '(1 2 3 4 5 6)))
   (is (= (my-flatten ["a" ["b"] "c"]) '("a" "b" "c")))
   (is (= (my-flatten '((((:a))))) '(:a))))
