2c36255c5cc32b618b2f1457de10eac8445d8053
Mario Giampietri <mario.giampietri@gmail.com>
2025-07-18 18:22:50 +0100

    python anagrams still WIP - iteration 2

--- python/anagrams.py
+++ python/anagrams.py
@@ -1,11 +1,20 @@
 # http://codekata.com/kata/kata06-anagrams/
+import unicodedata
+
+def normalize(word):
+    _w = "".join(sorted(word.lower().replace("'", "")))
+    return unicodedata.normalize('NFKD', _w).encode('ASCII', 'ignore').decode('ascii')
 
 def group_anagrams(words):
     anagrams_grouped = {}
     for w in words:
-        _w = "".join(sorted(w.lower()))
+        _w = normalize(w)
         if _w in anagrams_grouped:
             anagrams_grouped[_w].append(w)
         else:
             anagrams_grouped[_w] = [w]
-    return "\n".join(map(" ".join, anagrams_grouped.values()))
+    return list(anagrams_grouped.values())
+
+def group_anagrams_from_file(filepath):
+    with open(filepath, mode='r', buffering=-1, encoding="latin-1", errors=None, newline=None, closefd=True, opener=None) as f:
+        return group_anagrams(f.read().splitlines())
--- python/tests/anagrams_test.py
+++ python/tests/anagrams_test.py
@@ -4,13 +4,16 @@ import unittest
 class AnagramsTest(unittest.TestCase):
     def test_anagrams_from_list(self):
         words = ['boaster', 'boaters', 'borates', 'enlist', 'fresher', 'inlets', 'kinship', 'knits', 'listen', 'pinkish', 'refresh', 'rots', 'silent', 'sinks', 'skins', 'sort', 'stink']
-        expected = """boaster boaters borates
-enlist inlets listen silent
-fresher refresh
-kinship pinkish
-knits stink
-rots sort
-sinks skins"""
+        expected = [['boaster', 'boaters', 'borates'],
+                    ['enlist', 'inlets', 'listen', 'silent'],
+                    ['fresher', 'refresh'], ['kinship', 'pinkish'],
+                    ['knits', 'stink'],
+                    ['rots', 'sort'],
+                    ['sinks', 'skins']]
         self.assertEqual(expected, a.group_anagrams(words))
 
-# TODO: process (and run assertions against) whole anagram file from kata
+    # TODO: figure out what's wrong here (getting the expected count as per kata description) - main suspect is encoding in the file itself
+    def WIP_test_anagrams_from_file(self):
+        expected_anagram_count = 20683
+        a.group_anagrams_from_file("tests/wordlist.txt")
+        self.assertEqual(expected_anagram_count, len(a.group_anagrams_from_file("tests/wordlist.txt")))
